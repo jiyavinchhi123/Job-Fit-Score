fdc5eb02bf5874e7c83a3ff5b5bbf16d9fd8eda6
jiyavinchhi123 <jiya.vinchhi2412@gmail.com>
2026-04-05 16:47:03 +0530

    Delete frontend/js/env.js

--- frontend/js/env.js
+++
@@ -1,5 +0,0 @@
-// Set this to your deployed backend URL, e.g. "https://jobfit-backend.onrender.com"
-window.API_BASE = "https://jobfit-backend-gnr1.onrender.com";
-
-//localStorage.setItem("API_BASE","https://jobfit-backend-gnr1.onrender.com");
-//location.reload();
